5670aaec141553b24bb467fa386fb2ea3023c1a3
Mikael Lof <mickes511@hotmail.com>
2012-04-18 11:00:04 +0200

    updated Pom

--- test/src/test/java/RegisterLoadTest.java
+++ test/src/test/java/RegisterLoadTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import java.io.File;
 
 import static org.hamcrest.core.Is.is;
@@ -18,9 +16,9 @@ public class RegisterLoadTest {
 
     @Before
     public void dummyTest(){
-        FirefoxProfile profile = new FirefoxProfile();
 
-        driver = new FirefoxDriver(new FirefoxBinary(new File("C:\\tools\\Firefox/firefox.exe")), profile);
+
+        driver = new FirefoxDriver();
         driver.get("http://localhost:8080/LoadPlannerMain-1.0-SNAPSHOT");
     }
 
